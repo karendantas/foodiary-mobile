dcb353585327a739a8e06afb63dabec5ed5727c7
karendantas <karennobre74@gmail.com>
2025-07-20 10:54:34 -0300

    feat: start app and home components

---
+++ src/app/index.tsx
@@ -0,0 +1,69 @@
+import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold,
+  useFonts,
+} from '@expo-google-fonts/host-grotesk';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+import { useEffect } from 'react';
+import '../styles/global.css';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { HomeHeader } from '../components/HomeHeader';
+import { DateSwitcher } from '../components/DateSwitcher';
+import { DailyStats } from '../components/DailyStats';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [loaded, error] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+    HostGrotesk_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
+    <View className="flex-1 bg-white">
+        <SafeAreaProvider>
+            <HomeHeader />
+            <DateSwitcher />
+             <View className="mt-2">
+                <DailyStats
+                    calories={{
+                    current: 500,
+                    goal: 2500,
+                    }}
+                    proteins={{
+                    current: 2000,
+                    goal: 2500,
+                    }}
+                    carbohydrates={{
+                    current: 500,
+                    goal: 2500,
+                    }}
+                    fats={{
+                    current: 500,
+                    goal: 2500,
+                    }}
+                />
+            </View>
+
+        <View className="h-px bg-gray-200 mt-7" />
+        </SafeAreaProvider>
+    </View>
+  );
+}
